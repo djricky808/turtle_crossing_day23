5e6bec946ce3cb5fa7b3b4dbf77c70d92657c8ed
Richard Olpindo <djricky088@yahoo.com>
2026-05-11 13:47:59 -1000

    Created test car for position and movement

--- main.py
+++ main.py
@@ -9,19 +9,25 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 screen.title("Turtle Crossing")
 
+CAR_GENERATION_RATE = 6
+i = 0
+
 root = screen.getcanvas().winfo_toplevel()
 root.call('wm','attributes','.','-topmost','1')
 
 game_is_running = True
 
 player = Player()
+car = CarManager()
 
 #Listen for the "Up" keypress to move the turtle north
 screen.listen()
 screen.onkey(fun=player.move_turtle,key="Up")
 
 while game_is_running:
+    i += 1
     time.sleep(0.1)
+    car.move_car()
     screen.update()
 
 screen.exitonclick()
