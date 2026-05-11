84dc43701da4ba045c3bb6f07ad26200b278a8d7
Richard Olpindo <djricky088@yahoo.com>
2026-05-11 13:47:28 -1000

    Created a function for car movement

--- car_manager.py
+++ car_manager.py
@@ -8,6 +8,7 @@ CAR_ZONE = (-230, 230)
 
 class CarManager(Turtle):
     #Create cars that are 20px high by 40px wide that are randomly generated along the y-axis.
+    #No cars should be generated in the top and bottom 50px of the screen (Turtle safe zone)
     def __init__(self):
         super().__init__()
         self.penup()
@@ -15,6 +16,8 @@ class CarManager(Turtle):
         self.shapesize(stretch_wid=1, stretch_len=2)
         self.color(random.choice(COLORS))
         self.setposition(x=270, y=random.randint(CAR_ZONE[0],CAR_ZONE[1]))
-    #No cars should be generated in the top and bottom 50px of the screen (Turtle safe zone)
+
     #Move the cars from the right edge to the left edge of the screen
+    def move_car(self):
+        self.setposition(self.xcor()-MOVE_INCREMENT,self.ycor())
     #Generate a new car only every time 6th time the game loop runs
