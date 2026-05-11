a68eee1c8962aa065b9a3d854e430c2aaf95c802
Richard Olpindo <djricky088@yahoo.com>
2026-05-11 13:07:46 -1000

    Set up Turtle as the parent class and created a reset function

--- player.py
+++ player.py
@@ -1,6 +1,13 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -200)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
-class Player:
-    pass
+class Player(Turtle):
+    #Create the turtle player
+    def __init__(self):
+        super().__init__()
+
+    def reset_position(self):
+        self.position = (0, -200)
